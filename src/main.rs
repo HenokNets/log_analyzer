74ffbdab7a8948da6ed1d8ec7f1ccbde0c47cadf
HenokNets <henoknetsanet98@gmail.com>
2026-04-21 22:51:01 +0300

    feat: add basic file reading with clap args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+#[derive(Parser)]
+struct Args {
+    #[arg(long)]
+    path: String,
+}
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    
+    let file = File::open (args.path)?;
+
+    let reader = BufReader::new (file);
+
+    for line in reader.lines() {
+        let line = line?;
+        println!("{}", line);
+    }
+    
+
+    Ok(())
+    
 }
