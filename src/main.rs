116b18c572eb9147a25dbe8ede43876879b6b513
HenokNets <henoknetsanet98@gmail.com>
2026-04-23 07:33:32 +0300

    add hardcoded error filtering

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,29 @@ use std::io::{BufRead, BufReader};
 struct Args {
     #[arg(long)]
     path: String,
+    level: Option<String>,
 }
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     
-    let file = File::open (args.path)?;
+    let file: File = File::open (args.path)?;
 
     let reader = BufReader::new (file);
 
+    let mut count = 0;
+
     for line in reader.lines() {
         let line = line?;
-        println!("{}", line);
+        
+        if line.contains ("ERROR") {
+            println!("{}", line);
+             count+=1;
+            
+        }
+        
     }
+
+    println!("Total errors: {}", count);
     
 
     Ok(())
