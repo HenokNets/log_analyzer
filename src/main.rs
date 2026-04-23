f39563c403aea27fa2d1b1b4cf90520d5a64dbec
HenokNets <henoknetsanet98@gmail.com>
2026-04-23 08:26:23 +0300

    add optional log level filtering with clap

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,38 @@
 use clap::Parser;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+
 #[derive(Parser)]
 struct Args {
     #[arg(long)]
     path: String,
+
+    #[arg(long)]
     level: Option<String>,
 }
+
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
-    
-    let file: File = File::open (args.path)?;
 
-    let reader = BufReader::new (file);
+    let file = File::open(&args.path)?;
+    let reader = BufReader::new(file);
 
     let mut count = 0;
 
     for line in reader.lines() {
         let line = line?;
-        
-        if line.contains ("ERROR") {
+
+        if let Some(ref level) = args.level {
+            if line.contains(level) {
+                println!("{}", line);
+                count += 1;
+            }
+        } else {
             println!("{}", line);
-             count+=1;
-            
         }
-        
     }
 
-    println!("Total errors: {}", count);
-    
+    println!("Total matches: {}", count);
 
     Ok(())
-    
 }
